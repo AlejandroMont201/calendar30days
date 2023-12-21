6de9500cdd9299e17cd139aa825040247a32d43b
AlejandroMont201 <alejomont25@gmail.com>
2023-12-20 21:45:42 -0400

    commit2

--- app-calendario/src/App.jsx
+++ app-calendario/src/App.jsx
@@ -1,40 +1,72 @@
+// App.jsx
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import December21Page from './December21Page';
 
-function App() {
-  const generateButtonStyle = (index) => {
-    return index % 2 === 0 ? 'purple' : 'blue';
-  };
+const Home = () => (
+  <>
+    <h1 className="title">Calendario para Ariel</h1>
+    <h1 className="title">TE AMUU</h1>
+    <div className="cards-container">{renderButtons()}</div>
+    <div className="card"></div>
+  </>
+);
 
-  const renderButtons = () => {
-    const buttons = [];
+const DecemberPage = () => (
+  <>
+    <h1 className="title">Contenido de diciembre</h1>
+    {/* Agrega aquí el contenido específico de diciembre */}
+  </>
+);
 
-    for (let i = 21; i <= 51; i++) {
-      const buttonStyle = generateButtonStyle(i - 21);
-      const buttonText = i <= 31 ? `Diciembre` : `Enero`;
-      const buttonTextStyle = i <= 31 ? 'december-text' : 'january-text';
+const JanuaryPage = () => (
+  <>
+    <h1 className="title">Contenido de enero</h1>
+    {/* Agrega aquí el contenido específico de enero */}
+  </>
+);
 
-      buttons.push(
-        <div key={i} className="button-container">
-          <button className={`heart ${buttonStyle}`}>
-            {i <= 31 ? i : i - 31}
-          </button>
-          <div className={buttonTextStyle}>{buttonText}</div>
-        </div>
-      );
-    }
+const renderButtons = () => {
+  const buttons = [];
 
-    return buttons;
-  };
+  for (let i = 21; i <= 51; i++) {
+    const buttonStyle = i % 2 === 0 ? 'purple' : 'blue';
 
-  return (
-    <>
-      <h1 className="title">Calendario para Ariel</h1>
-      <h1 className="title">TE AMUU</h1>
-      <div className="cards-container">{renderButtons()}</div>
-      <div className="card"></div>
-    </>
-  );
-}
+    buttons.push(
+      <div key={i} className="button-container">
+        {i === 21 ? (
+          <Link to={`/december/${i}`}>
+            <button className={`heart ${buttonStyle}`}>
+              {i}
+            </button>
+          </Link>
+        ) : (
+          <Link to={i <= 31 ? `/december/${i}` : `/january/${i - 31}`}>
+            <button className={`heart ${buttonStyle}`}>
+              {i <= 31 ? i : i - 31}
+            </button>
+          </Link>
+        )}
+        <p className={i <= 31 ? 'december-text' : 'january-text'}>
+          {i <= 31 ? 'Diciembre' : 'Enero'}
+        </p>
+      </div>
+    );
+  }
+
+  return buttons;
+};
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/december/:day" element={<DecemberPage />} />
+      <Route path="/january/:day" element={<JanuaryPage />} />
+      <Route path="/december/21" element={<December21Page />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
